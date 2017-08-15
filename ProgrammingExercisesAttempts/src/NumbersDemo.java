be628d0cac66f825d61d3b4afb7b787bf7826498
BenjaminHerd <benoherdo@gmail.com>
2017-08-15 11:55:13 +1000

    Comments added and second method complete.

--- ProgrammingExercisesAttempts/src/NumbersDemo.java
+++ ProgrammingExercisesAttempts/src/NumbersDemo.java
@@ -15,25 +15,43 @@ public class NumbersDemo {
         displayTwiceTheNumber(numOne);
         displayTwiceTheNumber(numTwo);
 
-//        displayNumberPlusFive(numOne);
-//        displayNumberPlusFive(numTwo);
-//
+        //break up each method
+        System.out.println("--Next Method--");
+
+        displayNumberPlusFive(numOne);
+        displayNumberPlusFive(numTwo);
+
+        System.out.println("--Next Method--");
 //        displayNumberSquared(numOne);
 //        displayNumberSquared(numTwo);
+
+        System.out.println("--Finish--");
     }
 
+    //variable at the end is different from the name of the value passed in
+
+    //Displaying a variable twice
     public static void displayTwiceTheNumber(int result)
     {
         System.out.println(result);
         System.out.println(result);
     }
 
-    public static void displayNumberPlusFive()
+    //Displaying a number added to 5
+    public static void displayNumberPlusFive(int result)
     {
+        int adder;
+        int newResult;
+        adder = 5;
+        System.out.println(result + " + 5 equals:");
 
+        //Could probably just use 5 as well
+        newResult = result + adder;
+        System.out.println(newResult);
     }
 
-    public static void displayNumberSquared()
+    //squaring given number
+    public static void displayNumberSquared(int result)
     {
 
     }
